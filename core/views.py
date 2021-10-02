5918f5d17953b6b24ac6ceff9a9ad6f91d6280c6
Gustavo <dossantosgss@gmail.com>
2021-10-02 17:23:07 -0300

    implementado tela de Login com auth

--- core/views.py
+++ core/views.py
@@ -1,13 +1,36 @@
+from django.contrib import messages
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 # def index(request):
 #     return redirect("/agenda/")
 
+def login_user(request):
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect("/")
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('Username')
+        password = request.POST.get('Password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect("/")
+        else:
+            messages.error(request, "Usuário ou Senha Inválido")
+    return  redirect("/")
+
+@login_required(login_url="/login/")
 def lista_eventos(request):
     usuario = request.user
-    ##evento = Evento.objects.all()
+    #evento = Evento.objects.all()
     evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos':evento}
     return render(request,'agenda.html', dados)
