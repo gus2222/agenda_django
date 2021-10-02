32eb88788c58af76f02d3aa134c8fe366e7ad6d8
Gustavo <dossantosgss@gmail.com>
2021-10-02 16:33:56 -0300

    Separando templates

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 # Create your views here.
+
+# def index(request):
+#     return redirect("/agenda/")
+
+def lista_eventos(request):
+    usuario = request.user
+    ##evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
+    dados = {'eventos':evento}
+    return render(request,'agenda.html', dados)
 
 def eventos(request, titulo_evento):
     local = Evento.objects.get(titulo = titulo_evento).descricao
