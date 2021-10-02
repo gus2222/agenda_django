9b4a78612dfe655d2bc90c2fe47c39d71fe4bccb
Gustavo <dossantosgss@gmail.com>
2021-10-02 15:42:15 -0300

    url eventos que retorna o que tem no campo descricao do evento

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 # Create your views here.
+
+def eventos(request, titulo_evento):
+    local = Evento.objects.get(titulo = titulo_evento).descricao
+    return HttpResponse('<h2>O local do Evento {} é {} </h2>'.format(titulo_evento,local))
