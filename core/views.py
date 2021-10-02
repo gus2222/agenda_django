69cb3817c9cdce97f677177f2a7069429fc6f755
Gustavo <dossantosgss@gmail.com>
2021-10-02 18:15:20 -0300

    template de insercao de eventos na agenda e giignore

--- core/views.py
+++ core/views.py
@@ -38,3 +38,18 @@ def lista_eventos(request):
 def eventos(request, titulo_evento):
     local = Evento.objects.get(titulo = titulo_evento).descricao
     return HttpResponse('<h2>O local do Evento {} é {} </h2>'.format(titulo_evento,local))
+@login_required(login_url="/login")
+def evento(request):
+    return render(request, 'evento.html')
+@login_required(login_url="/login")
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento= data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect("/")
