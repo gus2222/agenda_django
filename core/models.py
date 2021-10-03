0f999be08dad93d9d0d7258a2b6cd92091b062f5
Gustavo <dossantosgss@gmail.com>
2021-10-03 12:15:13 -0300

    filtrando dados e resposta json

--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from datetime import datetime
 # Create your models here.
 
 class Evento(models.Model):
@@ -17,7 +17,13 @@ class Evento(models.Model):
         return self.titulo
 
     def get_data_evento(self):
-        return self.data_evento.strftime('%d/%m/%Y %H:%mh')
+        return self.data_evento.strftime('%d/%m/%Y %H:%Mh')
 
     def get_data_input_evento(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento < datetime.now():
+            return True
+        else:
+            return False
